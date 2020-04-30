e5f300a208f488ef0d2a044d4531e1abf80c0d34
jblanco <user>
2020-04-30 20:36:58 +0200

    Prueba initializer

--- config/initializers/create_configs.rb
+++ config/initializers/create_configs.rb
@@ -1,17 +1,17 @@
-General.create(
-    logo: "",
-    top_intro: "Intro",
-    top_current_party: "Próxima Sesión",
-    top_past_sessions: "Sesiones Pasadas",
-    top_contact: "Contacto & Redes",
-    contact_phone: "+34 666 77 88",
-    contact_email: "kream.madrid@gmail.com",
-    contact_facebook: "https://es-es.facebook.com/Kr3am/",
-    contact_instagram: "https://www.instagram.com/kream_madrid/?hl=es",
-    big_title: "psychodelic tribal session",
-    short_title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    video_background: "",
-    gif_background: "",
-    button_current_party: "Próxima Sesión",
-    button_past_sessions: "Sesiones Pasadas"
-    ) unless General.any?
+# General.create(
+#     logo: "",
+#     top_intro: "Intro",
+#     top_current_party: "Próxima Sesión",
+#     top_past_sessions: "Sesiones Pasadas",
+#     top_contact: "Contacto & Redes",
+#     contact_phone: "+34 666 77 88",
+#     contact_email: "kream.madrid@gmail.com",
+#     contact_facebook: "https://es-es.facebook.com/Kr3am/",
+#     contact_instagram: "https://www.instagram.com/kream_madrid/?hl=es",
+#     big_title: "psychodelic tribal session",
+#     short_title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+#     video_background: "",
+#     gif_background: "",
+#     button_current_party: "Próxima Sesión",
+#     button_past_sessions: "Sesiones Pasadas"
+#     ) unless General.any?
